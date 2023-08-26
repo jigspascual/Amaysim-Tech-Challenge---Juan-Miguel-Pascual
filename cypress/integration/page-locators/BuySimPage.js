dd41975e5a7003d5c03c9138f46095ce5477de5c
Jigs Pascual <juanmiguelpascual17@gmail.com>
2023-08-26 20:30:46 +0800

    Updated tests and added new Page Locator

--- cypress/integration/page-locators/BuySimPage.js
+++ cypress/integration/page-locators/BuySimPage.js
@@ -2,11 +2,23 @@ class BuySimPage{
     constructor(){
         this.radioButton = 'input[name="simType"]'
         this.numberSwitcher = 'div[data-testid="number-transfer-switcher"]'
+        this.checkOutButton = 'a[data-testid="product-checkout-button"]'
+    }
+    //Select SimPlan if USIM (Physical Sim) or ESIM (Electronic Sim)
+    selectSimType(simType){
+        cy.get(this.radioButton,{timeout:10000}).check(simType,{force:true});
+    }
+    //Select if user retain their number(yes) or opt for a new sim(no)
+    keepNumber(retainSim){
+        cy.get(this.numberSwitcher).find('input').check(retainSim,{force:true})
+    }
+    clickCheckoutButton(){
+        cy.get(this.checkOutButton).should('be.visible').click();
     }
     fillInDetails(simType,retainSim){
-        cy.get(this.radioButton,{timeout:10000}).check(simType,{force:true});
-        cy.get(this.numberSwitcher).find('input').check(retainSim,{force:true})
+        this.selectSimType(simType);
+        this.keepNumber(retainSim);
+        this.clickCheckoutButton();
       } 
-
 }
 export default BuySimPage;
--- cypress/integration/test-specs/AmaysimTest.spec.js
+++ cypress/integration/test-specs/AmaysimTest.spec.js
@@ -1,14 +1,19 @@
 import LandingPage from "../page-locators/LandingPage";
 import SevenDaySimPlanPage from "../page-locators/SevenDaySimPlanPage";
 import SimPlansPage from "../page-locators/SimPlansPage";
+import BuySimPage from "../page-locators/BuySimPage";
+import DetailsPage from "../page-locators/DetailsPage";
 describe('Sample', () =>{
     const myLandingPage = new LandingPage();
     const mySimPlansPage = new SimPlansPage();
-    const mySevenDaySimPlanPage = new SevenDaySimPlanPage();
+    const myBuySimPage = new BuySimPage();
+    const myDetailsPage = new DetailsPage();
     it('Sample', () =>{
         cy.visit('https://www.amaysim.com.au/');
         //myLandingPage.selectSimPlan2();
         myLandingPage.selectSimPlan("Data plans");
         mySimPlansPage.buySimPlan("Data only",1);
+        myBuySimPage.fillInDetails("USIM","no");
+        myDetailsPage.setDetails(false,"Jigs","Pascual","12101992","jigspascual@sample.com","jigspascual","mypassword245",false,"Level 6, 17-19 Bridge St, SYDNEY NSW 2000","Level 6, 17-19 Bridge St, SYDNEY NSW 2000",true);
     })
 })
